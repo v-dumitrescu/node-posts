b5f905a43b6deebb87d0b23205e9fbf08c357eb6
Valentin Dumitrescu <val3ntin.dumitrescu@gmail.com>
2024-02-14 17:09:02 +0200

    Add create functionality for posts

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 const mongoose = require('mongoose');
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost/node-posts')
 
 const Post = require('./models/Post');
 
+app.use(bodyParser.urlencoded({extended: false}));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
@@ -29,14 +31,34 @@ app.get('/posts', (req, res) => {
 });
 
 app.get('/posts/create', (req, res) => {
-  const title = 'Create Post';
+  const formTitle = 'Create Post';
   res.render('posts/post-form', {
-    title
+    formTitle,
+    method: 'POST'
   });
 });
 
 app.post('/posts/create', (req, res) => {
-  
+  const formTitle = 'Create Post';
+  const { title, content } = req.body;
+  if(!title.trim() || !content.trim()) {
+    res.render('posts/post-form', {
+      formTitle,
+      method: 'POST',
+      action: '/posts/create',
+      title,
+      content,
+      error: 'Both fields are required!'
+    });
+  } else {
+    new Post({
+      title,
+      content
+    })
+    .save()
+    .then(() => res.redirect('/posts'))
+    .catch(err => console.log(err));
+  }
 });
 
 app.get('/posts/edit/:id', (req, res) => {
